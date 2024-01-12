4e73f1cf81079955cff8060199735793d723695c
JhonMedina <jhon-43769@outlook.com>
2024-01-12 19:14:26 -0400

    Create Structure GiftApp

---
+++ src/app/gifs/components/search-box/search-box.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'gifs-search-box',
+  template: `
+    <h5>Buscar</h5>
+    <input type="text" class="form-controll" placeholder="Buscar..." />
+  `,
+})
+export class SearchBoxComponent {}
---
+++ src/app/gifs/pages/home/home-page.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'Gifs-home-page',
+  styleUrl: 'home-page.component.css',
+  templateUrl: './home-page.component.html',
+})
+export class HomePageComponent {}
