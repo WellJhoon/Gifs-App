7bbdb065a78d553d7ede2abf04739aa976eddc6d
JhonMedina <jhon-43769@outlook.com>
2024-04-01 16:46:03 -0400

    consuming GiffApi and normalize

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,7 +6,9 @@ import { Injectable } from '@angular/core';
 })
 export class GifsService {
   private _Taghistory: string[] = [];
-  constructor() {}
+  private apiKey: string = 'h3vKAUUqxs3RBvwK9764nk66pi6DAyKI';
+  private serviceURL: string = 'https://api.giphy.com/v1/gifs';
+  constructor(private http: HttpClient) {}
 
   get tagHistory() {
     return [...this._Taghistory];
@@ -23,5 +26,15 @@ export class GifsService {
   searchTag(tag: string): void {
     if (tag.length === 0) return;
     this.organizeHistory(tag);
+
+    const params = new HttpParams()
+      .set('api_key', this.apiKey)
+      .set('q', tag)
+      .set('limit', 10);
+    this.http
+      .get(`${this.serviceURL}/search`, { params: params })
+      .subscribe((resp: any) => {
+        console.log(resp.data);
+      });
   }
 }
